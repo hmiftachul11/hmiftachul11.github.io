b39d184f73fcdcb7becde0df1bf5dbb4e9ea2bf5
hmiftachul11 <hmiftachul11@gmail.com>
2022-05-18 11:52:47 +0700

    'portofolio'

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+// Navbar
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classLost.remove("navbar-fixed");
+  }
+};
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#0ea5e9",
+        secondary: "#9ca3af",
+        dark: "#1e293b",
+      },
+    },
+  },
+  plugins: [],
+};
